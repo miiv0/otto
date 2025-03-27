02aaf627adb7003eeb526fc0e262049b78261801
Justin Holt <83739998+justinholt@users.noreply.github.com>
2025-03-27 02:50:20 -0400

    Implement basic Discord bot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import { Client, GatewayIntentBits } from "discord.js";
 
+const run = async () => {
+  const token = process.env.OTTO_TOKEN;
+  const clientId = process.env.OTTO_CLIENT_ID;
+
+  if (!token) throw Error("Environment variable OTTO_TOKEN must be set.");
+  if (!clientId) throw Error("Environment variable OTTO_GUILD_ID must be set.");
+
+  const client = new Client({
+    intents: [GatewayIntentBits.GuildVoiceStates],
+  });
+
+  await client.login(token);
+};
+
+run();
