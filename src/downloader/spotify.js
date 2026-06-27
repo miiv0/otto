db8c06f8fbb7eda1638f3736feb689a5b2e3a536
miiv0 <minzana2002@gmail.com>
2026-06-27 17:58:00 -0400

    -

--- src/downloader/spotify.js
+++ src/downloader/spotify.js
@@ -4,4 +4,3 @@ import { fetch } from "undici"
 const spotify = spotify(fetch)
 
 const data = await spotify.getData("https://open.spotify.com/track/5OXVLqrKCofCk4TLRHFGu8?si=c9de5226024e4848");
-console.log(data)
